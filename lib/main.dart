ed67788f4a5b21a3ff93ced960fdd480735abe8d
maxiggle <clsyfriday@gmail.com>
2023-04-13 06:03:12 +0100

    style: :art: changed title
    changed title

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class AppTheme extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'PORTFOLIO',
+      title: 'PORTFOLIO WITH STORYBLOK',
       home: const RootScreen(),
     );
   }
